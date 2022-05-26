5c93589c6feb2f36f108a58faa67e205d5ad737c
Sandu Luca <sanduluca1@gmail.com>
2022-05-26 22:25:32 +0300

    created `Copyright` component

---
+++ components/Copyright.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from './Themed';
+
+
+export default function Copyright() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', padding: 4, marginTop: 16 }}>
+            <Text>&copy; Sandu Luca</Text>
+        </View>
+    );
+}
+
+
+
+
